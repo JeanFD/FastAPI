e88aca6642e2c2420d266b30e265eaa8c6ceeeec
Jean Dias <102756457+JeanFD@users.noreply.github.com>
2026-01-01 14:55:17 -0300

    parametros de consulta

--- documentacao/main.py
+++ documentacao/main.py
@@ -1,34 +1,47 @@
-from enum import Enum
+# parametros de consulta
 from fastapi import FastAPI
-
-class ModelName(str, Enum):
-    alexnet = "alexnet"
-    resnet = "resnet"
-    lenet = "lenet"
+from typing import Union
 
 app = FastAPI()
-# A ordem importa
-@app.get("/users/me")
-async def read_user_me():
-    return {"user_id": "the current user"}
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
 
-@app.get("/users/{user_id}")
-async def read_item(user_id: str):
-    return {"user_id": user_id}
+@app.get("/items/")
+async def read_item(skip: int = 0, limit: int = 10):
+    return fake_items_db[skip : skip + limit]
 
-# Valores predefinidos
+@app.get("/items/{item_id}")
+# async def read_item(item_id: int, q :Union[str, None] = None):
+async def read_item(item_id: str, q: str | None = None):
+    if q:
+        return {"item_id": item_id, "q": q}
+    return {"item_id": item_id}
 
-@app.get("/models/{model_name}")
-async def get_model(model_name: ModelName):
-    if model_name == ModelName.alexnet:
-        return {"model_name": model_name, "message": "Deep Learning FTW!"}
+@app.get("/items/{item_id}")
+async def read_item(item_id: str, q: str | None = None, short: bool = False):
+    item = {"item_id": item_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {"description": "This is an amazing item that has a long description"}
+        )
+    return item
 
-    if model_name.value == "lenet":
-        return {"model_name": model_name, "message": "LeCNN all the images"}
+@app.get("/users/{user_id}/items/{item_id}")
+async def read_user_item(
+    user_id: int, item_id: str, q: str | None = None, short: bool = False
+):
+    item = {"item_id": item_id, "owner_id": user_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {"description": "This is an amazing item that has a long description"}
+        )
+    return item
 
-    return {"model_name": model_name, "message": "Have some residuals"}
+@app.get("/items/{item_id}")
+async def read_user_item(item_id: str, needy: str):
+    item = {"item_id": item_id, "needy": needy}
+    return item
 
-# conversor de path
-@app.get("/files/{file_path:path}")
-async def read_file(file_path: str):
-    return {"file_path": file_path}
