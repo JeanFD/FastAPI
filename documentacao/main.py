3760b5fc9ee2221b952c9acfcaeb0b50985b6f7d
Jean Dias <102756457+JeanFD@users.noreply.github.com>
2026-01-01 17:51:59 -0300

    parametros de consulta e validacoes de string

--- documentacao/main.py
+++ documentacao/main.py
@@ -1,29 +1,47 @@
-from fastapi import FastAPI
-from pydantic import BaseModel
-
-class Item(BaseModel):
-    name: str
-    description: str | None = None
-    price: float
-    tax: float | None = None
-
+import random
+from fastapi import FastAPI, Query
+from typing import Annotated
+from pydantic import AfterValidator
 app = FastAPI()
 
-@app.post("/items/")
-async def create_item(item: Item):
-    item_dict = item.model_dump()
-    if item.tax is not None:
-        price_with_tax = item.price + item.tax
-        item_dict.update({"price_with_tax": price_with_tax})
-    return item_dict
+# @app.get("/items/")
+# async def read_items(q: Annotated[str | None, Query(min_length = 3, max_length=50, pattern="^fixedquery$")] = None):
+#     results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
+#     if q:
+#         results.update({"q": q})
+#     return results
 
-@app.put("/items/{item_id}")
-async def update_item(item_id: int, item: Item):
-    return {"item_id": item_id, **item.model_dump()}
+data = {
+    "isbn-9781529046137": "The Hitchhiker's Guide to the Galaxy",
+    "imdb-tt0371724": "The Hitchhiker's Guide to the Galaxy",
+    "isbn-9781439512982": "Isaac Asimov: The Complete Stories, Vol. 2",
+}
 
-@app.put("/items/{item_id}")
-async def uptade_item(item_id: int, item: Item, q: str | None = None):
-    result = {"item_id": item_id, **item.model_dump()}
-    if q:
-        result.update({"q": q})
-    return result
+def check_valid_id(id: str):
+    if not id.startswith(("isbn-", "imdb-")):
+        raise ValueError("Invalid ID format, it must start with 'isbn-' or 'imdb-'")
+    return id
+
+#  multiplos valores
+@app.get("/items/")
+async def read_items(
+    id: Annotated[
+        str | None,
+        AfterValidator(check_valid_id),
+        Query(
+            include_in_schema=False,
+            alias="item-query",
+            title="test3e",
+            description="Query string for the items to search in the database that have a good match", 
+            min_length=3,
+            max_length=50,
+            pattern="^fixedquery$",
+            deprecated=True
+        ),
+    ] = None,
+):
+    if id:
+        item = data.get(id)
+    else:
+        id, item = random.choice(list(data.items()))
+    return {"id": id, "item": item}
