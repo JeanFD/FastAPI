e1f3a69b01f6a74318a4d9098f6c5be0fa4f25b2
Jean Dias <102756457+JeanFD@users.noreply.github.com>
2026-01-02 14:03:57 -0300

    corpo - campos

--- documentacao/main.py
+++ documentacao/main.py
@@ -1,43 +1,27 @@
 from typing import Annotated
 
-from fastapi import FastAPI, Path, Body
-from pydantic import BaseModel
+from fastapi import Body, FastAPI
+from pydantic import BaseModel, Field
 
 app = FastAPI()
 
 class Item(BaseModel):
     name: str
-    description: str | None = None
-    price: float
+    description: str | None = Field(
+        default=None, 
+        title="The description of the item",
+          max_length=300
+    )
+    price: float = Field(
+        gt=0,
+        description="The price must be greater than zero"
+    )
     tax: float | None = None
 
-class User(BaseModel):
-    username: str
-    full_name: str | None = None
-
 @app.put("/items/{item_id}")
-async def update_item(
-    *,
-    item_id: Annotated[int, Path(title="the ID of the item to get", ge=0)],
-    q: str | None = None,
-    item: Item | None = None,
-    user: User | None = None,
-    importance: Annotated[int, Body(gt=0)],
-):
-    results = {"item_id": item_id}
-    if q:
-        results.update({"q": q})
-    if item:
-        results.update({"item": item})
-    if user:
-        results.update({"user": user})
-    return results
-
-@app.get("/items/{item_id}")
 async def update_item(
     item_id: int,
     item: Annotated[Item, Body(embed=True)]
 ):
     results = {"item_id": item_id, "item": item}
     return results
-
