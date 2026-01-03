6e42031d48db21887abda03c7aed599985fa517b
Jean Dias <102756457+JeanFD@users.noreply.github.com>
2026-01-03 14:13:29 -0300

    modelo de resposta - tipo de retorno

--- documentacao/main.py
+++ documentacao/main.py
@@ -1,21 +1,54 @@
-from typing import Annotated
+from typing import Any
 
-from fastapi import FastAPI, Header
-from pydantic import BaseModel
+from fastapi import FastAPI, Response
+from fastapi.responses import JSONResponse, RedirectResponse
+from pydantic import BaseModel, EmailStr
 
 app = FastAPI()
 
-class CommonHeaders(BaseModel):
-    model_config = {"extra": "forbid"}
+class Item(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    tax: float = 10.5
+    tags: list[str] = []
 
-    host: str
-    save_data: bool
-    if_modified_since: str | None = None
-    traceparent: str | None = None
-    x_tag: list[str] = []
+class BaseUser(BaseModel):
+    username: str
+    email: EmailStr
+    full_name: str | None = None
 
-@app.get("/items/")
-async def read_items(
-    headers: Annotated[CommonHeaders, Header(convert_underscores=False)]
-):
-    return headers
+class UserIn(BaseUser):
+    password: str
+
+items = {
+    "foo": {"name": "Foo", "price": 50.2},
+    "bar": {"name": "Bar", "description": "The bartenders", "price": 62, "tax": 20.2},
+    "baz": {"name": "Baz", "description": None, "price": 50.2, "tax": 10.5, "tags": []},
+}
+
+@app.post("/items/", response_model=Item)
+async def create_item(item: Item) -> Any:
+    return item
+
+@app.get("/items/", response_model=list[Item])
+async def read_items() -> Any:
+    return [
+        Item(name="Portal Gun", price=42.0),
+        Item(name="Plumbus", price=32.0),
+    ]
+
+@app.get("/items/{item_id}", Response_model=Item, response_model_exclude_unset=True)
+async def read_item(item_id: str):
+    return items[item_id]
+
+# Don't do this in procuction!
+@app.post("/user/")
+async def create_user(user: UserIn) -> BaseUser:
+    return user
+
+@app.get("/portal", response_model=None)
+async def get_portal(teleport: bool = False) -> Response | dict:
+    if teleport:
+        return RedirectResponse(url = "https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+    return {"message": "Here's your interdimensional portal."}
