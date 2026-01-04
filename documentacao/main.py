258fb5b5e77f68387229465bc8df2d92a175b241
Jean Dias <102756457+JeanFD@users.noreply.github.com>
2026-01-04 14:45:43 -0300

    modelos de formulario

--- documentacao/main.py
+++ documentacao/main.py
@@ -1,8 +1,15 @@
 from typing import Annotated
+
 from fastapi import FastAPI, Form
+from pydantic import BaseModel
 
 app = FastAPI()
 
+class FormData(BaseModel):
+    username: str
+    password: str
+    # model_config = {"extra": "forbid"}
+
 @app.post("/login/")
-async def login(username: Annotated[str, Form()], password: Annotated[str, Form()]):
-    return {"username": username}
+async def login(data: Annotated[FormData, Form()]):
+    return data
