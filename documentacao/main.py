9cc644de571bf7b0b0503ce2d7df204764cd30ed
Jean Dias <102756457+JeanFD@users.noreply.github.com>
2026-01-04 14:40:06 -0300

    dados do formulario

--- documentacao/main.py
+++ documentacao/main.py
@@ -1,7 +1,8 @@
-from fastapi import FastAPI, status
+from typing import Annotated
+from fastapi import FastAPI, Form
 
 app = FastAPI()
 
-@app.post("/items/", status_code=status.HTTP_201_CREATED)
-async def create_item(name: str):
-    return {"name": name}
+@app.post("/login/")
+async def login(username: Annotated[str, Form()], password: Annotated[str, Form()]):
+    return {"username": username}
