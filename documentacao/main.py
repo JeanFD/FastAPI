3e10b0e0d756e96186c21244c22ec9af78b253a9
Jean Dias <102756457+JeanFD@users.noreply.github.com>
2026-01-05 21:13:45 -0300

    manipulacao de erros

--- documentacao/main.py
+++ documentacao/main.py
@@ -1,17 +1,83 @@
-from typing import Annotated
+from fastapi import FastAPI, HTTPException, Request
+from fastapi.exception_handlers import (
+    http_exception_handler,
+    request_validation_exception_handler
+)
+from fastapi.encoders import jsonable_encoder
+from fastapi.exceptions import RequestValidationError
+from fastapi.responses import JSONResponse, PlainTextResponse
+from starlette.exceptions import HTTPException as StarletteHTTPException
+from pydantic import BaseModel
 
-from fastapi import FastAPI, File, Form, UploadFile
+
+class UnicornException(Exception):
+    def __init__(self, name: str):
+        self.name = name
 
 app = FastAPI()
 
-@app.post("/files/")
-async def create_file(
-    file: Annotated[bytes, File()],
-    fileb: Annotated[UploadFile, File()],
-    token: Annotated[str, Form()],
-):
-    return {
-        "file_size": len(file),
-        "token": token,
-        "fileb_content_type": fileb.content_type
-    }
+items = {"foo": "The Foo Wrestlers"}
+
+# @app.get("/items/{item_id}")
+# async def read_item(item_id: str):
+#     if item_id not in items:
+#         raise HTTPException(
+#             status_code = 404,
+#             detail="Item not found",
+#             headers={"X-Error": "There goes my error"},
+#             )
+#     return {"item": items[item_id]}
+
+@app.exeption_handler(UnicornException)
+async def unicorn_exception_handler(request: Request, exc: UnicornException):
+    return JSONResponse(
+        status_code = 418,
+        content = {
+            "message": f"Oops! {exc.name} did something. There goes a rainbow..."
+        },
+    )
+
+# @app.exception_handler(RequestValidationError)
+# async def validation_exception_gandler(request, exc: RequestValidationError):
+#     message = "Validation errors:"
+#     for error in exc.errors():
+#         message += f"\nField: {error['loc']}, Error: {error['msg']}"
+#     return PlainTextResponse(message, status_code=400)
+
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(request: Request, exc: RequestValidationError):
+    return JSONResponse(
+        status_code=422,
+        content=jsonable_encoder({"detail": exc.errors(), "body": exc.body}),
+    )
+
+class Item(BaseModel):
+    title: str
+    size: int
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: int):
+    if item_id == 3:
+        raise HTTPException(status_code=418, detail="Nope! I don't like 3.")
+    return {"item_id": item_id}
+
+@app.get("/unicorns/{name}")
+async def read_unicorn(name: str):
+    if name == "yolo":
+        raise UnicornException(name=name)
+    return {"unicorn_name": name}
+
+@app.post("/items/")
+async def create_item(item: Item):
+    return item
+
+
+@app.exception_handler(StarletteHTTPException)
+async def custom_http_exception_handler(request, exc):
+    print(f"OMG! An HTTP error!: {repr(exc)}")
+    return await http_exception_handler(request, exc)
+
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(request, exc):
+    print("OMG! The client sent invalid data!: {exc}")
+    return await request_validation_exception_handler(request, exc)
